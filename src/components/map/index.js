93ec34af79277b0bfbbe7be6036a6024e096d5a7
Ivan-Encinas <ivan.encinas@rootstrap.com>
2022-06-13 16:26:09 -0300

    User location

--- src/components/map/index.js
+++ src/components/map/index.js
@@ -1,22 +1,56 @@
-// eslint-disable-next-line no-unused-vars
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import marker from './icon';
+import { useEffect, useState } from 'react';
+
 import 'leaflet/dist/leaflet.css';
 import './styles.scss';
 
 const MapView = () => {
-  return (
-    <MapContainer center={{ lat: -34.483672, lng: -58.504768 }} zoom={13}>
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={[-34.483672, -58.504768]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
+  const [position, setPosition] = useState({
+    ready: false,
+    where: [],
+    error: null,
+  });
+  const geoOptions = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0,
+  };
+  const geolocationEnabled = () => {
+    setPosition({ ready: false, error: null });
+    return navigator.geolocation.getCurrentPosition(
+      position => {
+        geoSuccess(position);
+      },
+      () => {},
+      geoOptions
+    );
+  };
+
+  const geoSuccess = position => {
+    setPosition({
+      ready: true,
+      where: [position.coords.latitude, position.coords.longitude],
+    });
+  };
+
+  useEffect(() => {
+    return geolocationEnabled();
+  }, []);
+
+  return position.where?.length === 2 ? (
+    <MapContainer
+      className="map__container"
+      center={position.where}
+      zoom="20"
+      scrollWheelZoom={true}
+    >
+      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      <Marker position={position.where} icon={marker}>
+        <Popup>You are here.</Popup>
       </Marker>
     </MapContainer>
-  );
+  ) : null;
 };
 
 export default MapView;
--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import { useCallback, useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
