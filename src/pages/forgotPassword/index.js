4c4c7e21c7b34bfa583754612a610ae7612466c8
Ivan-Encinas <ivan.encinas@rootstrap.com>
2022-07-14 12:36:07 -0300

    PR updates

--- src/pages/forgotPassword/index.js
+++ src/pages/forgotPassword/index.js
@@ -8,6 +8,10 @@ import useTranslation from 'hooks/useTranslation';
 import { forgotPasswordSchema } from 'schemas';
 import routesPaths from 'routes/routesPaths';
 import { Link } from 'react-router-dom';
+import applestoreButton from 'assets/appstore-button.png';
+import twitterButton from 'assets/Twitter.png';
+import facebookButton from 'assets/Facebook.png';
+import phoneIcon from 'assets/i6Phone.svg';
 
 import './styles.scss';
 import { useResetPasswordMutation } from 'services/profile/profile';
@@ -16,27 +20,27 @@ const ForgotPassword = () => {
   const t = useTranslation();
   const {
     register: forgotPassowrd,
-    handleSubmit: handleOnSubmitForgotPassowrd,
+    handleSubmit: handleOnSubmitForgotPassword,
     formState: { errors: forgotPassowrdErrors },
   } = useForm({ resolver: zodResolver(forgotPasswordSchema) });
-  const [isSend, setIsSend] = useState(false);
+  const [isSent, setIsSent] = useState(false);
   const [resetPassword, { isSuccess }] = useResetPasswordMutation();
   const redirectUrl = process.env.REACT_APP_FORGOT_PASSWORD_URL;
-  const onSubmitForgotPassowrd = data => {
+  const onSubmitForgotPassword = data => {
     resetPassword({ ...data, redirect_url: redirectUrl });
   };
 
   useEffect(() => {
     if (isSuccess) {
-      setIsSend(true);
+      setIsSent(true);
     }
   }, [isSuccess]);
 
   return (
-    <div className="row">
-      {!isSend ? (
-        <div className="form column left-column">
-          <form onSubmit={handleOnSubmitForgotPassowrd(onSubmitForgotPassowrd)}>
+    <section className="row">
+      {!isSent ? (
+        <article className="form column left-column">
+          <form onSubmit={handleOnSubmitForgotPassword(onSubmitForgotPassword)}>
             <div className="circles"> </div>
             <h1 className="title">{t('forgotPassword.title')}</h1>
             <label htmlFor="email">{t('forgotPassword.email')}</label>
@@ -54,21 +58,29 @@ const ForgotPassword = () => {
               </Link>
             </div>
           </form>
-        </div>
+        </article>
       ) : (
         <div className="form column left-column">
           <h3>{t('forgotPassword.message')}</h3>
         </div>
       )}
-      <div className="column right-column">
-        <div className="i6"></div>
-        <button id="apple-store"></button>
-        <div className="social-media">
-          <button id="facebook"></button>
-          <button id="twitter"></button>
+      <article className="column right-column">
+        <div className="phone__section">
+          <img className="icon" src={phoneIcon} alt="Applestore Logo" />
         </div>
-      </div>
-    </div>
+        <a target="_blank" href="https://www.apple.com/la/app-store/" rel="noreferrer">
+          <img className="apple-store" src={applestoreButton} alt="Applestore Logo" />
+        </a>
+        <div className="social-media">
+          <a target="_blank" href="https://es-la.facebook.com/" rel="noreferrer">
+            <img className="facebook" src={facebookButton} alt="Facebook Logo" />
+          </a>
+          <a target="_blank" href="https://twitter.com/?lang=es" rel="noreferrer">
+            <img className="twitter" src={twitterButton} alt="Twitter Logo" />
+          </a>
+        </div>
+      </article>
+    </section>
   );
 };
 
